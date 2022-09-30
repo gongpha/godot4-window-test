b9d285f16c414bc4a15324c600ddf8fd2fc781cd
Kongfa Waroros <gongpha@hotmail.com>
2022-09-30 17:46:05 +0700

    renamed all unreadable names
    they think im a robot
    
    001 002 003 004

--- sub/cat_popup/cat_outside.gd
+++ sub/cat_popup/cat_outside.gd
@@ -1,24 +1,25 @@
 extends Sprite2D
 
-@export var normal : Texture
+@export var normal : Texture # reused cat texture (atlas)
 
 var win : Window
 var ground : float
 var hit_ground : bool = false
-const TASKBAR_HEIGHT := 48
+const TASKBAR_HEIGHT := 48 # i cant communicate with DWM (on windows). sad
 
 func _ready() :
-	win = $"../../"
+	win = $"../../" # link to Outside Window
 	set_process(false)
 	
+# start animating when jumeped
 func animate() :
 	win.visible = true
-	win.transparent = true
+	win.transparent = true # idk why needed to set it twice
 	ground = (win.size.y - texture.get_height() - (TASKBAR_HEIGHT * (win.content_scale_factor / 2.0))) / (win.content_scale_factor)
 	set_process(true)
 	
 func _process(delta : float) :
-	
+	# Checking is touched a taskbar area (the height is 48px in win11)
 	if position.y >= ground :
 		if !hit_ground :
 			texture = normal
@@ -28,16 +29,11 @@ func _process(delta : float) :
 		position.x += 32.0 * delta
 		frame = min(int(wrap(0, 16, position.x)) / 16 % 2, 1) # swap 0, 1
 		
+		# Walks until off-screen then quit
 		if position.x * win.content_scale_factor > win.size.x :
 			get_tree().quit()
 			return
 	else :
-		#print(position.y, ground)
+		# gravity
 		position.y += 80.0 * delta
 		position.x += 4.0 * delta
-	
-	
-
-
-func _on_001_timeout():
-	set_process(true)
--- sub/cat_popup/main.gd
+++ sub/cat_popup/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const WINDOW := preload("res://sub/000/001.tscn")
+const WINDOW := preload("res://sub/cat_popup/outside_window.tscn")
 
 var mainwin : Window
 var window : Window
@@ -10,17 +10,15 @@ func _ready() :
 	mainwin.transparent = true
 	mainwin.transparent_bg = true
 	
-const SAFE_RECT := Vector2i(300, 80)
+const SAFE_RECT := Vector2i(300, 80) # for seamlessly continuing motion
 
 func spawn_window() :
 	window = WINDOW.instantiate()
 	add_child(window, true)
-	
-	
 
 func window_animate() :
 	var res := DisplayServer.screen_get_size()
 	window.position = (mainwin.position + mainwin.size) - SAFE_RECT
 	window.size = res - window.position
-	window.get_node("000/000").animate()
+	window.get_node("scene/cat").animate()
 	mainwin.grab_focus()
